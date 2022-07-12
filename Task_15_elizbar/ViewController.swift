a7cc51d6dc243de01e3651b96a82a9cfd34e6d26
elizbar <elizbar>
2022-07-12 13:35:20 +0400

    Initial Commit

---
+++ Task_15_elizbar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task_15_elizbar
+//
+//  Created by alta on 7/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
